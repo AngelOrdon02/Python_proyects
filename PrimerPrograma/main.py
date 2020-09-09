5aa3797cb041146e879de7c5affab3b61a0d73a3
Angel <3006613200101@ingenieria.usac.edu.gt>
2020-09-08 20:23:12 -0600

    Mi primer commit

---
+++ PrimerPrograma/main.py
@@ -0,0 +1,29 @@
+# Comentario de una sola linea
+'''
+Comentario multilinea
+Una linea
+dos lineas
+tres lineas
+'''
+if 5 > 2:
+    print("Cinco es mayor a dos")
+
+# Asignacion de valor para declarar una variable
+x = 5
+y = "Hello, World!"
+
+nombreVariable = x
+
+print(nombreVariable)
+print(y)
+
+# De aqui para abajo realizo los cambios para mi primer commit con Kraken
+
+x = 100
+y = 50
+
+if x > y:
+    resultado = x // y
+    print("El resultado de " + str(x) + " dividido " + str(y) + " es:  " + str(resultado))
+
+print("Fin del programa :D")
